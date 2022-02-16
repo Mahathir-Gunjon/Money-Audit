6869a1ffb78e63416fd5a12ba5d360608538bfbb
Mahathir-Gunjon <mahathirgunjon@gmail.com>
2022-02-17 04:13:24 +0600

    function ctreated

--- js/app.js
+++ js/app.js
@@ -1,21 +1,54 @@
-document.getElementById('calculation-button').addEventListener('click', function() {
+function expenseAndBalance(){
+    
     const foodInput = document.getElementById('food-input').value;
-    const food = parseFloat(foodInput);
-    const rantInput = document.getElementById('rent-input').value;
-    const rant = parseFloat(rantInput);
+    const rentInput = document.getElementById('rent-input').value;
     const clothInput = document.getElementById('clothe-input').value;
-    const cloth = parseFloat(clothInput);
 
-    const total = food + rant + cloth;
+    const total = parseFloat(foodInput) + parseFloat(rentInput) + parseFloat(clothInput);
     const totalOutput = document.getElementById('expense-total');
     const totalExpense = totalOutput.innerText;
     totalOutput.innerText = total;
 
     const incomeInput = document.getElementById('income-input');
     const income = incomeInput.value;
-    const totalBalanse = income - total;
+    
+    const totalBalance = income - total;
 
     const balance = document.getElementById('balance-total');
-    balance.innerText = totalBalanse;
+    balance.innerText = totalBalance;
+    //conditions
+    if(total > income || totalBalance == 0){
+        alert('You are in debt');
+        return balance.innerText = '00';     
+    }
+    else if(typeof incomeInput != 'number'){
+        alert('Please enter a number');
+    }
+}
+
+
+
+
+document.getElementById('calculation-button').addEventListener('click', function() {
+    expenseAndBalance()
+}) 
+
+/* document.getElementById('calculation-button').addEventListener('click', function() {
     
-})
+    const foodInput = document.getElementById('food-input').value;
+    const rentInput = document.getElementById('rent-input').value;
+    const clothInput = document.getElementById('clothe-input').value;
+
+    const total = parseFloat(foodInput) + parseFloat(rentInput) + parseFloat(clothInput);
+    const totalOutput = document.getElementById('expense-total');
+    const totalExpense = totalOutput.innerText;
+    totalOutput.innerText = total;
+
+    const incomeInput = document.getElementById('income-input');
+    const income = incomeInput.value;
+    const totalBalance = income - total;
+
+    const balance = document.getElementById('balance-total');
+    balance.innerText = totalBalance;
+    
+}) */
