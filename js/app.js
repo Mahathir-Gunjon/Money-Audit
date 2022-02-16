eded15795c22573a16a19a30f626d519884849f3
Mahathir-Gunjon <mahathirgunjon@gmail.com>
2022-02-17 02:44:48 +0600

    javascript event lesener has been created

--- js/app.js
+++ js/app.js
@@ -1,1 +1,21 @@
-console.log('banaitei hobe')
+document.getElementById('calculation-button').addEventListener('click', function() {
+    const foodInput = document.getElementById('food-input').value;
+    const food = parseFloat(foodInput);
+    const rantInput = document.getElementById('rent-input').value;
+    const rant = parseFloat(rantInput);
+    const clothInput = document.getElementById('clothe-input').value;
+    const cloth = parseFloat(clothInput);
+
+    const total = food + rant + cloth;
+    const totalOutput = document.getElementById('expense-total');
+    const totalExpense = totalOutput.innerText;
+    totalOutput.innerText = total;
+
+    const incomeInput = document.getElementById('income-input');
+    const income = incomeInput.value;
+    const totalBalanse = income - total;
+
+    const balance = document.getElementById('balance-total');
+    balance.innerText = totalBalanse;
+    
+})
