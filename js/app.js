28a7784df49345def0884d64d2e0c34f646cc8c4
Mahathir-Gunjon <mahathirgunjon@gmail.com>
2022-02-17 20:12:05 +0600

    error masage handled

--- js/app.js
+++ js/app.js
@@ -1,37 +1,52 @@
-function expenseAndBalance(){
+// function to calculate the total balance
+function expenseAndBalance(balanceMoney, ) {
     
     const foodInput = document.getElementById('food-input').value;
     const rentInput = document.getElementById('rent-input').value;
     const clothInput = document.getElementById('clothe-input').value;
-
+    // total expenses
     const total = parseFloat(foodInput) + parseFloat(rentInput) + parseFloat(clothInput);
-    const totalOutput = document.getElementById('expense-total');
-    const totalExpense = totalOutput.innerText;
-    totalOutput.innerText = total;
-
+    const totalExpenseOutput = document.getElementById('expense-total');
+    totalExpenseOutput.innerText = total;
+    // total balance
     const incomeInput = document.getElementById('income-input');
     const income = incomeInput.value;
-    
     const totalBalance = income - total;
-
-    const balance = document.getElementById('balance-total');
+    const balance = document.getElementById(balanceMoney);
     balance.innerText = totalBalance;
-    //conditions
-    if(total > income || totalBalance == 0){
-        alert('You are in debt');
-        return balance.innerText = '00';     
+    // error handling start here
+    //conditions to check if the user has enough money to buy the item
+    if(foodInput.value == '' || rentInput.value == '' || clothInput.value == ''){
+        alert('Please enter valid expensed amount');
+        foodInput.style.color = 'red';
     }
-    else if(typeof incomeInput != 'number'){
-        alert('Please enter a number');
+    else if(incomeInput.value <= 0 && Number.isInteger(incomeInput.value) == false || incomeInput.value == ''){
+        alert('Please enter valid cash amount');
+    }
+    else if(totalBalance < 0){
+    alert('Your don\'t have enough money to pay your expenses');   
     }
 }
+function saveMoney(moneySave){
+    //saving amount from the balamce
+    const savingInput = document.getElementById(moneySave).value;
+    const saving = document.getElementById('saving-amount');
+    saving.innerText = parseFloat(savingInput) -;
 
-
-
-
+  
+}
 document.getElementById('calculation-button').addEventListener('click', function() {
-    expenseAndBalance()
+    expenseAndBalance('balance-total')
 }) 
+
+document.getElementById('saving').addEventListener('click', function() {
+    saveMoney('saving-input')
+})
+
+
+
+
+
 
 /* document.getElementById('calculation-button').addEventListener('click', function() {
     
@@ -40,9 +55,9 @@ document.getElementById('calculation-button').addEventListener('click', function
     const clothInput = document.getElementById('clothe-input').value;
 
     const total = parseFloat(foodInput) + parseFloat(rentInput) + parseFloat(clothInput);
-    const totalOutput = document.getElementById('expense-total');
-    const totalExpense = totalOutput.innerText;
-    totalOutput.innerText = total;
+    const totalExpenseOutput = document.getElementById('expense-total');
+    const totalExpense = totalExpenseOutput.innerText;
+    totalExpenseOutput.innerText = total;
 
     const incomeInput = document.getElementById('income-input');
     const income = incomeInput.value;
@@ -50,5 +65,11 @@ document.getElementById('calculation-button').addEventListener('click', function
 
     const balance = document.getElementById('balance-total');
     balance.innerText = totalBalance;
-    
-}) */
+
+    document.getElementById('saving').addEventListener('click', function() {
+        const savingInput = document.getElementById('saving-input').value;
+        const saving = parseFloat(savingInput) % parseFloat(totalBalance);
+        console.log(saving);
+    })
+})
+ */
